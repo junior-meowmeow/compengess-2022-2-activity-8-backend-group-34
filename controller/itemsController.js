d9415c28c41accaa6ccc224fb6e43ae374236b99
RujjiraP <Rujjirap@gmail.com>
2023-03-22 12:26:19 +0700

    hello

--- controller/itemsController.js
+++ controller/itemsController.js
@@ -25,24 +25,58 @@ exports.getGroupMembers = async (req, res) => {
 
 // TODO #1.1: Get items from DynamoDB
 exports.getItems = async (req, res) => {
-  // You should change the response below.
-  res.send("This route should get all items in DynamoDB.");
+
+  try {
+    // console.log(await docClient.send(new ScanCommand(scanParams)))
+    const data = await docClient.send(new ScanCommand({
+      TableName: process.env.aws_items_table_name,
+    }));
+    console.log(data.Items)
+    res.send(data.Items);
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(400);
+  }
 };
+
 
 // TODO #1.2: Add an item to DynamoDB
 exports.addItem = async (req, res) => {
+  console.log(req.body)
   const item_id = uuidv4();
   const created_date = Date.now();
   const item = { item_id: item_id, ...req.body, created_date: created_date };
-
-  // You should change the response below.
-  res.send("This route should add an item in DynamoDB.");
+  const putParams = {
+    Item: item,
+    TableName: process.env.aws_items_table_name,
+  };
+  try {
+    const response = await docClient.send(new PutCommand(putParams));
+    console.log(response);
+    res.send("Added");
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(400);
+  }
 };
+
 
 // TODO #1.3: Delete an item from DynamDB
 exports.deleteItem = async (req, res) => {
   const item_id = req.params.item_id;
-
-  // You should change the response below.
-  res.send("This route should delete an item in DynamoDB with item_id.");
+  const deleteParams = {
+    TableName: process.env.aws_items_table_name,
+    Key: {
+      item_id: item_id,
+    },
+  };
+  try {
+    const response = await docClient.send(new DeleteCommand(deleteParams));
+    console.log(response);
+    res.send("Delete item successfully");
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(400);
+  }
 };
+
