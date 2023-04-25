c0c567826ccd0f090e65ab2d9d0949dc2ab56614
junior-meowmeow <121653436+junior-meowmeow@users.noreply.github.com>
2023-04-26 02:25:59 +0700

    update after activity

--- controller/itemsController.js
+++ controller/itemsController.js
@@ -26,6 +26,7 @@ exports.getGroupMembers = async (req, res) => {
 // TODO #1.1: Get items from DynamoDB
 exports.getItems = async (req, res) => {
   // You should change the response below.
+  //res.send("This route should get all items in DynamoDB.");
   const params = {
     TableName: process.env.aws_items_table_name,
   };
@@ -44,36 +45,42 @@ exports.addItem = async (req, res) => {
   const created_date = Date.now();
   const item = { item_id: item_id, ...req.body, created_date: created_date };
 
-  // You should change the response below.
   const params = {
     TableName: process.env.aws_items_table_name,
     Item: item,
   };
+
   try {
     const data = await docClient.send(new PutCommand(params));
-    res.send(data.Items);
+    res.send(data);
   } catch (err) {
     console.error(err);
     res.status(500).send(err);
   }
+
+  // You should change the response below.
+  //res.send("This route should add an item in DynamoDB.");
 };
 
 // TODO #1.3: Delete an item from DynamDB
 exports.deleteItem = async (req, res) => {
   const item_id = req.params.item_id;
 
-  // You should change the response below.
   const params = {
     TableName: process.env.aws_items_table_name,
     Key: {
       item_id: item_id,
     },
   };
+
   try {
     const data = await docClient.send(new DeleteCommand(params));
-    res.send(data.Items);
+    res.send(data);
   } catch (err) {
     console.error(err);
     res.status(500).send(err);
   }
+
+  // You should change the response below.
+  //res.send("This route should delete an item in DynamoDB with item_id.");
 };
